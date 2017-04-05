e56722beee374f18a3fb6460759d8fd8ab8055a5
kadnikov <akadnikov@gmail.com>
2017-04-05 12:47:03 +0300

    CMIS headers 1

--- cmis/src/main/java/ru/doccloud/cmis/server/MyCmisBrowserBindingServlet.java
+++ cmis/src/main/java/ru/doccloud/cmis/server/MyCmisBrowserBindingServlet.java
@@ -1,6 +1,7 @@
 package ru.doccloud.cmis.server;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
