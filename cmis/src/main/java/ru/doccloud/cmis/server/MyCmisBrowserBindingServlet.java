c5a74e52b78bf137fb5b95f99cfb3c9cd47ccaf3
Andrey Kadnikov <akadnikov@gmail.com>
2017-04-07 10:40:22 +0300

    Base host from header

--- cmis/src/main/java/ru/doccloud/cmis/server/MyCmisBrowserBindingServlet.java
+++ cmis/src/main/java/ru/doccloud/cmis/server/MyCmisBrowserBindingServlet.java
@@ -1,6 +1,7 @@
 package ru.doccloud.cmis.server;
 
 import java.io.IOException;
+import java.util.Enumeration;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -28,8 +29,23 @@ public class MyCmisBrowserBindingServlet extends CmisBrowserBindingServlet {
 	    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException,
 	            IOException {
 			 LOG.debug("LOGIN CALLED");
-
-		 request.setAttribute(Dispatcher.BASE_URL_ATTRIBUTE, "http://doccloud.ru/jooq/browser");
+//			 LOG.debug("x-forwarded-proto header - "+request.getHeader("x-forwarded-proto"));
+//			 LOG.debug("getServerName - "+request.getServerName());
+//			 LOG.debug("getServerPort - "+request.getServerPort());
+//			 LOG.debug("host header - "+request.getHeader("host"));
+//			 Enumeration<String> headerNames = request.getHeaderNames();
+//
+//		        if (headerNames != null) {
+//		                while (headerNames.hasMoreElements()) {
+//					String headerName = headerNames.nextElement();
+//		                        LOG.debug("Header: "+ headerName+ " - " + request.getHeader(headerName));
+//		                }
+//		        }
+        String host = request.getHeader("host");
+        if (request.getHeader("x-forwarded-host")!=null){
+        	host = request.getHeader("x-forwarded-host");
+        }
+		 request.setAttribute(Dispatcher.BASE_URL_ATTRIBUTE, "http://"+host+"/jooq/browser");
 		 super.service(request, response);
 	 
 	 }
