608aea49eef7972ba384b942d7ca0a8464d0abf9
Andrey Kadnikov <akadnikov@gmail.com>
2017-07-18 18:11:05 +0300

    IA API simulation

---
+++ doccontrollers/src/main/java/ru/doccloud/document/controller/IAController.java
@@ -0,0 +1,111 @@
+package ru.doccloud.document.controller;
+
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import ru.doccloud.common.util.VersionHelper;
+import ru.doccloud.service.document.dto.DocumentDTO;
+import ru.doccloud.service.DocumentCrudService;
+import ru.doccloud.service.DocumentSearchService;
+import ru.doccloud.storage.storagesettings.StorageAreaSettings;
+import ru.doccloud.storagemanager.StorageManager;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Andrey Kadnikov
+ */
+@RestController
+@RequestMapping("/restapi/systemdata")
+public class IAController  extends AbstractController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IAController.class);
+
+    private final DocumentCrudService crudService;
+
+    private final DocumentSearchService searchService;
+
+    @Autowired
+    public IAController(DocumentCrudService crudService, DocumentSearchService searchService,
+                              StorageAreaSettings storageAreaSettings, StorageManager storageManager) throws Exception {
+        super(storageAreaSettings, storageManager);
+        LOGGER.info("DocumentController(crudService={}, searchService = {}, storageAreaSettings= {}, storageManager={})", crudService, searchService, storageAreaSettings, storageManager);
+        this.crudService = crudService;
+        this.searchService = searchService;
+
+        LOGGER.info("DocumentController(): storage settings {}", settingsNode);
+
+    }
+
+    @RequestMapping(value = "/tenants", method = RequestMethod.GET)
+    public List<DocumentDTO> findTenants() {
+        LOGGER.info("findTenants");
+        Long parentid = Long.parseLong("0");
+        return crudService.findAllByParent(parentid);
+    }
+    
+    @RequestMapping(value = "/tenants/{tenantId}/applications", method = RequestMethod.GET)
+    public List<DocumentDTO> findAplications(@PathVariable("tenantId") String tenantId) {
+        LOGGER.info("findApplications");
+        Long parentid = Long.parseLong(tenantId);
+        return crudService.findAllByParent(parentid);
+    }
+    
+    @RequestMapping(value = "/type/{type}", method = RequestMethod.GET)
+    public Page<DocumentDTO> findByType(@PathVariable("type") String type, @RequestParam(value = "fields",required=false) String fields, @RequestParam(value = "filters",required=false) String query,Pageable pageable) {
+        LOGGER.info("findByType(type = {}, fields={}, query={}, pageSize= {}, pageNumber = {})",
+                type, fields, query,
+                pageable.getPageSize(),
+                pageable.getPageNumber()
+        );
+        String[] fieldsArr = null;
+        if (fields!=null){
+        	fieldsArr = fields.split(",");
+        }
+
+        return crudService.findAllByType(type, fieldsArr, pageable, query);
+    }
+
+    @RequestMapping(value = "/parent/{parentid}", method = RequestMethod.GET)
+    public List<DocumentDTO> findByParent(@PathVariable("parentid") Long parentid) {
+        LOGGER.info("findByParent(parentid = {})", parentid);
+
+        return crudService.findAllByParent(parentid);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public DocumentDTO findById(@PathVariable("id") Long id) {
+        LOGGER.info("findById(id= {})", id);
+
+        return crudService.findById(id);
+    }
+
+    @RequestMapping(value = "/uuid/{uuid}", method = RequestMethod.GET)
+    public DocumentDTO findByUUID(@PathVariable("uuid") String uuid) {
+        LOGGER.info("findByUUID(uuid= {})", uuid);
+
+        return crudService.findByUUID(uuid);
+    }
+
+    void setUser(){
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+        LOGGER.info("httpservlet request from setUser {} ", request);
+        if(request == null)
+            crudService.setUser();
+        else
+            crudService.setUser(request.getRemoteUser());
+    }
+
+}
