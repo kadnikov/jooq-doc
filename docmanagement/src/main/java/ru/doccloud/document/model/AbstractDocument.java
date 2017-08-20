3acd79c3bdaf58ae2a62ed063e9a71005271c6b9
ILya <iluaushakov79@gmail.com>
2017-08-20 14:55:46 +0200

    spring boot

--- docmanagement/src/main/java/ru/doccloud/document/model/AbstractDocument.java
+++ docmanagement/src/main/java/ru/doccloud/document/model/AbstractDocument.java
@@ -1,17 +1,13 @@
 package ru.doccloud.document.model;
 
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.UUID;
-
-import org.joda.time.LocalDateTime;
-
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.joda.time.LocalDateTime;
+
+import java.io.*;
+import java.util.UUID;
 
 /**
  * Created by ilya on 5/27/17.
@@ -58,14 +54,14 @@ public abstract class AbstractDocument  implements Serializable{
             out.writeBoolean(false);
         } else {
             out.writeBoolean(true);
-            new ObjectMapper().configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false).writeValue(out, this.data);
+            new ObjectMapper().configure(JsonGenerator.Feature.AUTO_CLOSE_TARGET, false).writeValue((OutputStream) out, this.data);
         }
     }
 
     private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
         in.defaultReadObject();
         if(in.readBoolean()){
-            this.data = new ObjectMapper().configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, false).readValue(in, JsonNode.class);
+            this.data = new ObjectMapper().configure(JsonParser.Feature.AUTO_CLOSE_SOURCE, false).readValue((InputStream) in, JsonNode.class);
         }     
     }
 
