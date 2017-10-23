eb510a2f0552cf1adb75742fad18cb142fa8f42e
Andrey Kadnikov <akadnikov@gmail.com>
2017-10-23 11:46:01 +0300

    Cache config - connection error handling.

--- config/src/main/java/ru/doccloud/config/CachingConfiguration.java
+++ config/src/main/java/ru/doccloud/config/CachingConfiguration.java
@@ -1,9 +1,15 @@
 package ru.doccloud.config;
 
+import java.util.Arrays;
+
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCache;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,16 +28,46 @@ public class CachingConfiguration extends CachingConfigurerSupport {
         ClientConfig config = new ClientConfig(); 
         ClientNetworkConfig networkConfig = config.getNetworkConfig();
         networkConfig.addAddress("hazelcast");
-        networkConfig.setConnectionAttemptLimit(Integer.MAX_VALUE);
+        networkConfig.setConnectionAttemptLimit(5);
         networkConfig.setConnectionAttemptPeriod(10000);
         networkConfig.setConnectionTimeout(5000);
-
-        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
-        return new HazelcastCacheManager(client); 
+        try {
+	        HazelcastInstance client = HazelcastClient.newHazelcastClient(config);
+	        return new HazelcastCacheManager(client); 
+        }catch (IllegalStateException e) {
+        	SimpleCacheManager cacheManager = new SimpleCacheManager();
+            cacheManager.setCaches(Arrays.asList(new ConcurrentMapCache("docsByType"), new ConcurrentMapCache("userByLoginAndPwd")));
+            return cacheManager;
+		}
     } 
     @Bean
     @Override
     public KeyGenerator keyGenerator() {
         return null;
     }
+	@Override
+	public CacheErrorHandler errorHandler() {
+	     return new CacheErrorHandler() {
+	            @Override
+	            public void handleCacheGetError(RuntimeException exception, Cache cache, Object key) {
+	                System.out.println("cache get error");
+	            }
+
+	            @Override
+	            public void handleCachePutError(RuntimeException exception, Cache cache, Object key, Object value) {
+	                System.out.println("cache put error");
+	            }
+
+	            @Override
+	            public void handleCacheEvictError(RuntimeException exception, Cache cache, Object key) {
+	                System.out.println("cache evict error");
+	            }
+
+	            @Override
+	            public void handleCacheClearError(RuntimeException exception, Cache cache) {
+	                System.out.println("cache clear error");
+	            }
+	     };
+	}
+    
 }
